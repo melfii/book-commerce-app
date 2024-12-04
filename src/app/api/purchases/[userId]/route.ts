a127721b060301e2ddc918621bf330efc5988106
melfi <melfissa0806@gmail.com>
2024-12-04 16:44:22 +0900

    done

--- src/app/api/purchases/[userId]/route.ts
+++ src/app/api/purchases/[userId]/route.ts
@@ -4,15 +4,14 @@ import { NextResponse } from "next/server";
 //https://nextjs.org/docs/app/building-your-application/routing/route-handlers#dynamic-route-segments
 export async function GET(
   request: Request,
-  { params }: { params: { userId: string } }
+  { params }: { params: Promise<{ userId: string }> }
 ) {
-  const userId = params.userId;
+  const userId = (await params).userId;
 
   try {
     const purchase = await prisma.purchase.findMany({
       where: { userId: userId },
     });
-    console.log(purchase);
 
     return NextResponse.json(purchase);
   } catch (err) {
