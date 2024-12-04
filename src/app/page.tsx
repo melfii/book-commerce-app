b1846746c98600fc5ff3a2a8e718cc4e11c228fd
melfi <melfissa0806@gmail.com>
2024-12-04 16:37:40 +0900

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,24 @@
+import { getServerSession } from "next-auth";
 import Book from "./components/Book";
 import { getAllBooks } from "./lib/microcms/client";
-import { BookType } from "./types/types";
+import { BookType, Purchase, User } from "./types/types";
+import { nextAuthOptions } from "./lib/next-auth/options";
 
 export default async function Home() {
 
   const { contents } = await getAllBooks();
-  // const session = await getServerSession(nextAuthOptions);
-  // const user = session?.user as User;
-  const  purchaseBookIds: string[] = [];
+  const session = await getServerSession(nextAuthOptions);
+  const user = session?.user as User;
+  let purchaseBookIds: string[] = [];
 
-  // if(user) {
-  //   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/purchases/${user.id}`);
-  //   const purchasesData = await response.json();
+  if(user) {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/purchases/${user.id}`);
+    const purchasesData = await response.json();
 
-  //   purchaseBookIds = purchasesData.map(
-  //     (purchaseBook: Purchase) => purchaseBook.bookId 
-  //   );
-  // }
+    purchaseBookIds = purchasesData.map(
+      (purchaseBook: Purchase) => purchaseBook.bookId 
+    );
+  }
 
   return (
     <main className="flex flex-wrap justify-center items-center md:mt-32 mt-20">
