166b1630d6eade2fe211da4a8920417c39a1ed11
melfi <melfissa0806@gmail.com>
2024-12-04 17:13:51 +0900

    done

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,7 +1,12 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: "2024-11-20.acacia" });
+let stripe: Stripe;
+
+if(process.env.STRIPE_SECRET_KEY) {
+    stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+    
+}
 
 export async function POST(request: Request) {
 
