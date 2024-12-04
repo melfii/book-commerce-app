c8264640d1ab1375711eca907e91d9119c1e782f
melfi <melfissa0806@gmail.com>
2024-12-04 12:57:38 +0900

    done

--- src/app/api/purchases/[id]/route.ts
+++ src/app/api/purchases/[id]/route.ts
@@ -1,23 +1,23 @@
-// import prisma from "@/app/lib/prisma";
-// import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/app/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
 
-// // 購入履歴検索API
-// export async function GET(
-//     request: NextRequest,
-//     { params }: { params: { id: string } }) {
+// 購入履歴検索API
+export async function GET(
+    request: NextRequest,
+    { params }: { params: { id: string } }) {
 
-//         const { id } = await params;
+        const { id } = await params;
 
-//         try {
-//             const purchases = await prisma.purchase.findMany({
-//                 where: {
-//                     userId: id
-//                 }
-//             })
+        try {
+            const purchases = await prisma.purchase.findMany({
+                where: {
+                    userId: id
+                }
+            })
 
-//             return NextResponse.json(purchases);
-//         } catch(error) {
-//             return NextResponse.json(error);
-//         }
+            return NextResponse.json(purchases);
+        } catch(error) {
+            return NextResponse.json(error);
+        }
 
-// }
+}
