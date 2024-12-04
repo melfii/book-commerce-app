7a671c3b557a8793c8ed7db09aefec70d4d122e0
melfi <melfissa0806@gmail.com>
2024-12-04 12:38:10 +0900

    done

--- src/app/api/purchases/[id]/route.ts
+++ src/app/api/purchases/[id]/route.ts
@@ -4,14 +4,14 @@ import { NextRequest, NextResponse } from "next/server";
 // 購入履歴検索API
 export async function GET(
     request: NextRequest,
-    { params }: { params: { userId: string } }) {
+    { params }: { params: { id: string } }) {
 
-        const { userId } = await params
+        const { id } = await params
 
         try {
             const purchases = await prisma.purchase.findMany({
                 where: {
-                    userId: userId
+                    userId: id
                 }
             })
 
