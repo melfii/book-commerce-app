8a3f250426c9bb0034ee5fc0c4be4d03eaa25699
melfi <melfissa0806@gmail.com>
2024-12-04 17:15:49 +0900

    done

--- src/app/api/checkout/success/route.ts
+++ src/app/api/checkout/success/route.ts
@@ -2,7 +2,11 @@ import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+let stripe: Stripe;
+
+if(process.env.STRIPE_SECRET_KEY) {
+    stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+}
 
 // 購入履歴の保存
 export async function POST(request: Request) {
