ecb37921686a56555526a655b5f4ad02a72c9856
melfi <melfissa0806@gmail.com>
2024-12-04 13:07:33 +0900

    done

--- src/app/api/purchases/[userId]/route.ts
+++ src/app/api/purchases/[userId]/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 
 //https://nextjs.org/docs/app/building-your-application/routing/route-handlers#dynamic-route-segments
 export async function GET(
-  request: Request,
   { params }: { params: { userId: string } }
 ) {
   const userId = params.userId;
@@ -12,10 +11,9 @@ export async function GET(
     const purchase = await prisma.purchase.findMany({
       where: { userId: userId },
     });
-    console.log(purchase);
 
     return NextResponse.json(purchase);
-  } catch (err) {
-    return NextResponse.json(err);
+  } catch (error) {
+    return NextResponse.json(error);
   }
 }
