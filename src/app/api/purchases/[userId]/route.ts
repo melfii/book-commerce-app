bbd3f0290ea13451957a2c5b3b2c8a512a2162e5
melfi <melfissa0806@gmail.com>
2024-12-04 12:33:33 +0900

    done

--- src/app/api/purchases/[userId]/route.ts
+++ src/app/api/purchases/[userId]/route.ts
@@ -2,28 +2,28 @@ import prisma from "@/app/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-    request: NextRequest,
-    { params }: { params: { userId: string } }
-  ) {
-    const { userId } = params;
-  
-    try {
-      // 購入履歴の取得
-      const purchases = await prisma.purchase.findMany({
-        where: {
-          userId: userId,
-        },
-      });
-  
-      return NextResponse.json(purchases);
-    } catch (error) {
-      // エラーのログを記録
-      console.error("Error fetching purchases:", error);
-  
-      // クライアントに汎用エラーメッセージを返す
-      return NextResponse.json(
-        { message: "Failed to fetch purchase history" },
-        { status: 500 }
-      );
-    }
+  request: NextRequest,
+  context: { params: { userId: string } }
+) {
+  const { userId } = context.params;
+
+  try {
+    // 購入履歴の取得
+    const purchases = await prisma.purchase.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+
+    return NextResponse.json(purchases);
+  } catch (error) {
+    // エラーのログを記録
+    console.error("Error fetching purchases:", error);
+
+    // クライアントに汎用エラーメッセージを返す
+    return NextResponse.json(
+      { message: "Failed to fetch purchase history" },
+      { status: 500 }
+    );
   }
+}
