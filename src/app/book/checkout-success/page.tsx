a482b5a1ac3c1f893846385e2189df72d83deb86
melfi <melfissa0806@gmail.com>
2024-12-04 12:26:21 +0900

    done

--- src/app/book/checkout-success/page.tsx
+++ src/app/book/checkout-success/page.tsx
@@ -31,7 +31,7 @@ const PurchaseSuccess = () => {
             }
         }
         fetchData();
-    }, []);
+    }, [sessionId]);
 
   return (
     <div className="flex items-center justify-center mt-20">
--- src/app/components/Book.tsx
+++ src/app/components/Book.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { BookType } from "../types/types";
+import { BookType, User } from "../types/types";
 import { useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
@@ -15,7 +15,7 @@ type BookProps = {
 const Book = ({ book, isPurchased }: BookProps) => {
   const [showModal, setShowModal] = useState(false);
   const { data: session } = useSession();
-  const user: any = session?.user;
+  const user = session?.user as User;
   const router = useRouter();
 
   const handleCancel = () => {
