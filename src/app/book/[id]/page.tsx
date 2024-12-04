b639b244d80cef1094bd27c6182fd9d4068d8775
melfi <melfissa0806@gmail.com>
2024-12-04 16:46:37 +0900

    done

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -2,9 +2,9 @@ import { getDetailBook } from "@/app/lib/microcms/client";
 import Image from "next/image";
 import React from "react";
 
-const DetailBook = async ({ params }: { params: { id: string } }) => {
+const DetailBook = async ({ params }: { params: Promise<{ id: string }> }) => {
 
-    const { id } = await params;
+    const id = (await params).id;
     const book = await getDetailBook(id);
 
   return (
